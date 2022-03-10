869bc442067ab64ca172d51ea7a1bb5aefcfc6ab
videx <odejinmiabraham@gmail.com>
2022-03-10 16:43:05 +0100

    fingerprint sdk

--- app/src/main/java/com/nibsssdk/MainActivity.java
+++ app/src/main/java/com/nibsssdk/MainActivity.java
@@ -33,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 //                dispatchTakePictureIntent();
-            Intent in = new Intent(getApplicationContext(), BeginfingerprintActivity.class);
-//            Intent in = new Intent(getApplicationContext(), BegincaptureActivity.class);
+//            Intent in = new Intent(getApplicationContext(), BeginfingerprintActivity.class);
+            Intent in = new Intent(getApplicationContext(), BegincaptureActivity.class);
                 startActivity(in);
 //                finish();
             }
